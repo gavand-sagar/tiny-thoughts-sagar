53b6b57d490abf10bfdafd4147e61d18ceebd267
gavand-sagar <gavand.sagar.s@gmail.com>
2023-04-04 00:32:04 +0530

    array added for buttons

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../shared/components/Header/Header'
 import RoundedButton from '../../shared/components/RoundedButton/RoundedButton'
 
 export default function Home() {
+    const [categories, setCategories] = useState([
+        "React",
+        "Javascript",
+        "HTML",
+        "CSS",
+        "node.js"
+    ])
     return (
         <>
             <Header />
             <div className='app-container'>
                 <div className='sidebar'>
-                    <RoundedButton>react</RoundedButton>
-                    <RoundedButton>react</RoundedButton>
-                    <RoundedButton>react</RoundedButton>
-                    <RoundedButton>react</RoundedButton>
+                    {
+                        categories.map(x=><RoundedButton>{x}</RoundedButton>)
+                    }
                 </div>
                 <div className='main-content'>
 
